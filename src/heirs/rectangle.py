aa5bdcf50bd526038b6d33762cc00c6120bb2bd5
Satyrus <satyrus@inbox.ru>
2022-11-23 18:12:28 +0300

    hw-2 рефакторинг rectangle.py

--- src/heirs/rectangle.py
+++ src/heirs/rectangle.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from src.parents.polygon import *
+from collections import Counter
 
 
 class Rectangle(Polygon):
@@ -18,6 +19,40 @@ class Rectangle(Polygon):
 
     def add_area(self, figure):
         return self.calculate_area() + figure
+
+    @staticmethod
+    def check_count_elem(list_val):
+        return len(list_val)
+
+    @staticmethod
+    def check_is_not_zero(list_val):
+        return [i for i in list_val if i == 0]
+
+    @staticmethod
+    def check_is_num(list_val):
+        num = (int, float)
+        return [i for i in list_val if type(i) not in num]
+
+    @staticmethod
+    def check_less_than_zero(list_val):
+        return [i for i in list_val if i < 0]
+
+    def sum_angles_triangle(self):
+        return sum(self.angles)
+
+    @staticmethod
+    def check_repeating_elem(list_val):
+        """
+        В прямоугольнике 2 пары сторон с одинаковыми длинами [10, 20, 10, 20] -> (side_1: 2,side_1: 2) -> len() == 2
+        и summ == 4
+        :param list_val: Список длин сторон прямоугольника
+        :return:
+        number_of_pairs: количество пар Counter({10: 2, 20: 2})
+        sum_values: сумма значений (side_1: 2,side_1: 2)
+        """
+        sum_values = sum(Counter(list_val).values())
+        number_of_pairs = len(Counter(list_val))
+        return number_of_pairs, sum_values
 
 
 if __name__ == "__main__":
